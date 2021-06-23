40c7f8d0767eb4c9d26a1c9a5501e73241c3ddc0
subject026 <sudobuildspeakers@gmail.com>
2021-06-23 12:54:22 +0100

    boom

--- cypress/integration/indexPage.spec.js
+++ cypress/integration/indexPage.spec.js
@@ -3,24 +3,33 @@ import "cypress-axe";
 // beforeEach(() => {
 // });
 
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
-  });
+describe("Accessibility tests", () => {
+  beforeEach(() => {
+    cy.visit("/").get("main").injectAxe()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.checkA11y()
+  })
+})
+
+// describe("My First Test", () => {
+//   it("Does not do much!", () => {
+//     expect(true).to.equal(true);
+//   });
   
-  it("Homepage a11y", () => {
-    cy.visit("http://localhost:8000");
-    cy.injectAxe();
-    cy.checkA11y();
-    // cy.configureAxe({
-    //   branding: {
-    //     brand: String,
-    //     application: String
-    //   },
-    //   reporter: "option",
-    //   checks: [Object],
-    //   rules: [Object],
-    //   locale: Object
-    // });
-  });
-});
+//   it("Homepage a11y", () => {
+//     cy.visit("http://localhost:8000");
+//     cy.injectAxe();
+//     cy.checkA11y();
+//     // cy.configureAxe({
+//     //   branding: {
+//     //     brand: String,
+//     //     application: String
+//     //   },
+//     //   reporter: "option",
+//     //   checks: [Object],
+//     //   rules: [Object],
+//     //   locale: Object
+//     // });
+//   });
+// });
